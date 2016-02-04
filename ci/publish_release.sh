3dccdafe724088195f53a5f3e07495e25c1881c9
Karl Isenberg <karl@mesosphere.io>
2016-02-04 15:36:22 -0800

    Merge build branch in publish_release.sh

--- ci/publish_release.sh
+++ ci/publish_release.sh
@@ -11,8 +11,14 @@ fi
 
 project_dir=$(cd "$(dirname "${BASH_SOURCE}")/.." && pwd -P)
 
-if ! grep -q "dcos-centos-virtualbox-${BOX_VERSION}.box" "${project_dir}/metadata.json"; then
-  echo "metadata.json does not include dcos-centos-virtualbox-${BOX_VERSION}.box" >&2
+BOX_NAME=dcos-centos-virtualbox-${BOX_VERSION}
+
+echo "Checking out build branch: ${BOX_NAME}"
+git checkout ${BOX_NAME}
+git pull origin ${BOX_NAME}
+
+if ! grep -q "${BOX_NAME}.box" "${project_dir}/metadata.json"; then
+  echo "metadata.json does not include ${BOX_NAME}.box" >&2
   exit 1
 fi
 
@@ -22,3 +28,15 @@ docker build -t mesosphere/aws-cli .
 
 echo "Uploading metadata.json"
 aws-cli/aws.sh s3 cp metadata.json s3://dcos-vagrant/ --content-type "application/json"
+
+echo "Checking out master branch"
+git checkout master
+git pull origin master
+
+echo "Merging build branch to master"
+git merge ${BOX_NAME} --no-ff -m "Publish release: ${BOX_NAME}"
+git push origin master
+
+echo "Deleting build branch"
+git branch -D ${BOX_NAME}
+git push origin --delete ${BOX_NAME}
