296f8eea585f654608079325ebd9e39cfedc35b9
Karl Isenberg <karl@mesosphere.io>
2016-02-12 17:48:35 -0800

    Switch to Minimal CentOS install (smaller box size)

--- bin/dcos-deps.sh
+++ bin/dcos-deps.sh
@@ -4,6 +4,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+echo ">>> Installing packages required by DCOS installer"
+# See https://github.com/mesosphere/dcos-image/blob/master/providers/bash.py
+yum install --assumeyes --tolerant curl bash ping tar xz unzip
+
 echo ">>> Installing net-tools (for debugging)"
 yum install --assumeyes --tolerant net-tools
 
