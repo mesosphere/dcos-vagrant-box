adf0f00395a9ca96625697e705b328e76c1dbded
Karl Isenberg <karl@mesosphere.io>
2016-04-27 18:36:45 -0700

    Disable NetworkManager and clean network interfaces
    - Workaround bug where eth0 may be disabled on cloned VMs

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -13,3 +13,21 @@ rm -rf VBoxGuestAdditions_*.iso
 
 echo ">>> Cleaning Ruby Gems"
 rm -rf /tmp/rubygems-*
+
+echo ">>> Cleaning Network Interfaces"
+rm -f /etc/udev/rules.d/70-persistent-net.rules;
+mkdir -p /etc/udev/rules.d/70-persistent-net.rules;
+rm -f /lib/udev/rules.d/75-persistent-net-generator.rules;
+rm -rf /dev/.udev/;
+
+echo ">>> Disabling NetworkManager"
+for ndev in `ls -1 /etc/sysconfig/network-scripts/ifcfg-*`; do
+  if [ "$(basename ${ndev})" != "ifcfg-lo" ]; then
+    # clear MAC address
+    sed -i '/^HWADDR/d' "${ndev}";
+    # clear interface UID so it's regenerated for each new VM
+    sed -i '/^UUID/d' "${ndev}";
+    # disable NetworkManager
+    echo "NM_CONTROLLED=no" | tee --append "${ndev}" > /dev/null
+  fi
+done
